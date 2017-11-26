a267ab1454f7913013ac857b15d8921a509c7069
Ashish Bhatia <inspiremeashish@gmail.com>
2017-11-26 22:29:59 +0530

    Create Index.php
    Index file for sample php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+echo "<h1>Server Name:  <br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo "<h1>Http Host:  <br>";
+echo $_SERVER['HTTP_HOST'];
+echo "<br>";
+echo "<h1>Script URI: <br>";
+echo $_SERVER['SCRIPT_URI'];
+echo "<br>";
+echo "<h1>Script Name: <br>";
+echo $_SERVER['SCRIPT_NAME'];
+echo "<br>";
+echo "<h1>Server Address: <br>";
+echo $_SERVER['SERVER_ADDR'];
+echo "<br>";
+echo "<h1>Server Port: <br>";
+echo $_SERVER['SERVER_PORT'];
+?>
