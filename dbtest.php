b52fd7db2fa05c0869c327db0dfb733653375d49
Ashish Bhatia <inspiremeashish@gmail.com>
2017-11-27 14:16:02 +0530

    create db.test

---
+++ dbtest.php
@@ -0,0 +1,16 @@
+<?php
+
+$dbhost = getenv("MYSQL_SERVICE_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_SERVICE_PORT");
+$dbuser = 'root';
+$dbpwd = getenv("MYSQL_ROOT_PASSWORD");
+$dbnme = getenv("MYSQL_DATABASE");
+
+echo $dbhost;
+echo "<br>";
+echo $dbport;
+echo "<br>";
+echo $dbpwd;
+echo "<br>";
+echo $dbnme;
+>
