626c0078bd3ea6df4f3da51285c53b128b4c4201
Ashish Bhatia <inspiremeashish@gmail.com>
2017-11-27 18:11:41 +0530

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -1,13 +1,13 @@
 <?php
 
 echo "<h1>Server Name:  <br>";
-$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbhost = "localhost";
 $dbport = getenv("MYSQL_SERVICE_PORT");
 $dbuser = getenv("MYSQL_USER");
 $dbpwd = getenv("MYSQL_PASSWORD");
 $dbnme = getenv("MYSQL_DATABASE");
 // Create connection
-$conn = new mysqli($dbhost.":".$dbport, $dbuser, $dbpwd ,$dbnme);
+$conn = new mysqli($dbhost, $dbuser, $dbpwd ,$dbnme);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
