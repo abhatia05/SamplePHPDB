1201e4cedaf2fec061c9d7eaf877a8dad6ed32d9
Ashish Bhatia <inspiremeashish@gmail.com>
2017-11-27 17:53:43 +0530

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -7,7 +7,7 @@ $dbuser = getenv("MYSQL_USER");
 $dbpwd = getenv("MYSQL_PASSWORD");
 $dbnme = getenv("MYSQL_DATABASE");
 // Create connection
-$conn = new mysqli($dbhost, $dbuser, $dbpwd);
+$conn = new mysqli($dbhost, $dbuser, $dbpwd ,$dbnme);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
